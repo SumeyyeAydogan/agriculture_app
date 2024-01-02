c86c0c6eb02eebb26eaad316f24251d415577790
SumeyyeAydogan <aydogannsumeyye@gmail.com>
2024-01-02 23:01:00 +0300

    added core file

---
+++ lib/core/constants/application_constants.dart
@@ -0,0 +1,7 @@
+class ApplicationConstants {
+  static const String APP_NAME = "Agriculture App";
+  static const String TO_DO_QUESTION = "Bugün Ne Yapacaksın?";
+  static const String ADD_TASK = "Haydi Görev Ekleyelim";
+  static const String TYPE_TASK = "Görevi Yaz";
+  static const String VALIDATE_FORM_ERROR = "Lütfen en az 3 karakter giriniz";
+}
